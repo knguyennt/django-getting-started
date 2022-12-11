48d86231f49f17c56ccdd25ca90e0472d73110a0
knguyennt <khoanguyennt2010@gmail.com>
2022-12-11 22:11:04 +0700

    Update form

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import modelform_factory
 
 from .models import Meeting, Room
@@ -16,5 +16,12 @@ def room_listing(request):
 MeetingForm = modelform_factory(Meeting, exclude=[])
 
 def new(request):
-    form = MeetingForm()
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save() # save the instance to the database
+            return redirect("welcome")
+
+    else:
+        form = MeetingForm()
     return render(request, "meetings/new.html", {'form': form})
